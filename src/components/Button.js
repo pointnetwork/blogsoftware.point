af38ef770b061d24fba5f2e1ee6f3b1d0442b100
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-07-26 20:20:23 +0530

    Make button 'disable'able

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
-const PrimaryButton = ({ children, onClick }) => {
+const PrimaryButton = ({ children, disabled = false, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className='p-2 text-sm bg-indigo-500 text-white px-4 rounded hover:bg-indigo-700 active:bg-indigo-900 transition-all'
+      disabled={disabled}
+      className='p-2 text-sm font-medium bg-indigo-500 text-white px-4 rounded hover:bg-indigo-700 active:bg-indigo-900 transition-all disabled:bg-gray-300 disabled:cursor-not-allowed'
     >
       {children}
     </button>
   );
 };
 
-const OutlinedButton = ({ children, onClick }) => {
+const OutlinedButton = ({ children, disabled = false, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className='p-2 text-sm bg-white border border-indigo-500  text-indigo-500 px-4 rounded hover:text-indigo-700 active:text-indigo-900 hover:border-indigo-700 active:border-indigo-900 transition-all'
+      disabled={disabled}
+      className='p-2 text-sm font-medium bg-white border border-indigo-500  text-indigo-500 px-4 rounded hover:text-indigo-700 active:text-indigo-900 hover:border-indigo-700 active:border-indigo-900 transition-all disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-not-allowed'
     >
       {children}
     </button>
   );
 };
 
-const ErrorButton = ({ children, onClick }) => {
+const ErrorButton = ({ children, disabled = false, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className='p-2 text-sm bg-red-500 text-white px-4 rounded hover:bg-red-700 active:bg-red-900 transition-all'
+      disabled={disabled}
+      className='p-2 text-sm font-medium bg-red-500 text-white px-4 rounded hover:bg-red-700 active:bg-red-900 transition-all disabled:bg-gray-300 disabled:cursor-not-allowed'
     >
       {children}
     </button>
