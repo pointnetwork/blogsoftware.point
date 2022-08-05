bfdc1bc54b7c482c9b0b36bc7ff75fb4d82563d9
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-08-05 08:56:53 +0530

    Separate out blog posts related state and methods into a separate file to clean up the context

---
+++ src/context/useBlogs.ts
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { BlogContract } from '../@types/enums';
+import { Blog, BlogContractData, BlogsState } from '../@types/interfaces';
+import utils from './utils';
+
+const useBlogs = () => {
+  const [blogs, setBlogs] = useState<BlogsState>({
+    loading: true,
+    data: [],
+  });
+
+  const getAllBlogs = async () => {
+    setBlogs((prev) => ({ ...prev, loading: true }));
+
+    const { data }: { data: any[] } = await window.point.contract.call({
+      contract: BlogContract.name,
+      method: BlogContract.getAllBlogs,
+    });
+    const blogs = await Promise.all(
+      data.map(async (contractData) => {
+        const [
+          id,
+          storageHash,
+          isPublished,
+          publishDate,
+          previousStorageHashes,
+        ] = contractData;
+        const data = await utils.getDataFromStorage(storageHash);
+        return {
+          ...data,
+          id,
+          storageHash,
+          isPublished,
+          publishDate,
+          previousStorageHashes: previousStorageHashes.reverse(),
+        } as Blog & BlogContractData;
+      })
+    );
+    setBlogs({ loading: false, data: blogs });
+  };
+
+  const getDeletedBlogs = async () => {
+    const { data }: { data: any[] } = await window.point.contract.call({
+      contract: BlogContract.name,
+      method: BlogContract.getDeletedBlogs,
+    });
+    const blogs = await Promise.all(
+      data.map(async (contractData) => {
+        const [id, storageHash, isPublished, publishDate] = contractData;
+        const data = await utils.getDataFromStorage(storageHash);
+        return { ...data, id, storageHash, isPublished, publishDate } as Blog &
+          BlogContractData;
+      })
+    );
+    return blogs;
+  };
+
+  return { blogs, getAllBlogs, getDeletedBlogs };
+};
+
+export default useBlogs;
